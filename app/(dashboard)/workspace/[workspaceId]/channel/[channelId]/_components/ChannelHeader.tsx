75adec7b26e370f92437d89f17473c327348439b
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-10-30 13:16:56 +0200

    5:Invintations

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/ChannelHeader.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/ChannelHeader.tsx
@@ -1,11 +1,19 @@
 import { ModeToggle } from "@/components/theme-provider/ThemeSwitcher";
 import React from "react";
+import InviteMember from "./member/InviteMember";
+import MembersOverview from "./member/MembersOverview";
+interface ChnnelHeaderProp {
+  channelName: string | undefined;
+}
 
-const ChannelHeader = () => {
+const ChannelHeader = ({ channelName }: ChnnelHeaderProp) => {
   return (
     <div className="flex items-center justify-between h-14 px-4 border-b">
-      <h1 className="text-lg font-semibold"># Channel Header</h1>
-      <div className="flex items-center space-x-2">
+      <h1 className="text-lg font-semibold">{channelName}</h1>
+
+      <div className="flex items-center space-x-3">
+        <MembersOverview />
+        <InviteMember />
         <ModeToggle />
       </div>
     </div>
---
+++ app/router/member.ts
@@ -0,0 +1,83 @@
+import z from "zod";
+import { heavyWriteSecurityMiddleware } from "../middlewares/arcjet/heavy-write";
+import { standartWriteSecurityMiddleware } from "../middlewares/arcjet/standart-write";
+import { requiredAuthMiddleware } from "../middlewares/auth";
+import { base } from "../middlewares/base";
+import { requiredWorkspaceMiddleware } from "../middlewares/workspaceMiddleware";
+import { inviteMemberSchema } from "../schemas/member";
+import {
+  init,
+  organization_user,
+  Organizations,
+  Users,
+} from "@kinde/management-api-js";
+import { getAvatar } from "@/lib/helpers";
+import { readSecurityMiddleware } from "../middlewares/arcjet/read";
+
+export const inviteMember = base
+  .use(requiredAuthMiddleware)
+  .use(requiredWorkspaceMiddleware)
+  .use(standartWriteSecurityMiddleware)
+  .use(heavyWriteSecurityMiddleware)
+  .route({
+    method: "POST",
+    path: "/workspace/members/invite",
+    summary: "Invite Member",
+    tags: ["Members"],
+  })
+  .input(inviteMemberSchema)
+  .output(z.void())
+  .handler(async ({ context, input, errors }) => {
+    try {
+      init();
+
+      await Users.createUser({
+        requestBody: {
+          organization_code: context.workspace.orgCode,
+          profile: {
+            given_name: input.name,
+            picture: getAvatar(null, input.email),
+          },
+          identities: [
+            {
+              type: "email",
+              details: {
+                email: input.email,
+              },
+            },
+          ],
+        },
+      });
+    } catch {
+      throw errors.INTERNAL_SERVER_ERROR();
+    }
+  });
+
+export const listMembers = base
+  .use(requiredAuthMiddleware)
+  .use(requiredWorkspaceMiddleware)
+  .use(readSecurityMiddleware)
+  .route({
+    method: "GET",
+    path: "/workspace/members",
+    summary: "List all members",
+    tags: ["Members"],
+  })
+  .input(z.void())
+  .output(z.array(z.custom<organization_user>()))
+  .handler(async ({ context, errors }) => {
+    try {
+      init();
+
+      const data = await Organizations.getOrganizationUsers({
+        orgCode: context.workspace.orgCode,
+        sort: "name_asc",
+      });
+
+      if (!data.organization_users) throw errors.NOT_FOUND();
+
+      return data.organization_users;
+    } catch {
+      throw errors.INTERNAL_SERVER_ERROR();
+    }
+  });
